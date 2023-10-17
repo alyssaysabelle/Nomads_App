924eff92415da56af121e148bc3b5d0a00062da5
Richard <94967357+lcdApple@users.noreply.github.com>
2023-10-17 12:42:20 +0800

    Add View Other Profile
    -add its activity
    -add its xml

---
+++ app/src/main/java/com/mobdeve/s13/group8/nomadsapp/ViewOtherProfile.java
@@ -0,0 +1,14 @@
+package com.mobdeve.s13.group8.nomadsapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ViewOtherProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_other_profile);
+    }
+}
