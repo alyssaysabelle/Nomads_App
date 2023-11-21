b0d05b272e46fb6a1d3bbb460db135657ddb0f21
Richard <94967357+lcdApple@users.noreply.github.com>
2023-11-21 17:34:08 +0800

    Add upload image in register
    -add upload image in register
    -fix xml

--- app/src/main/java/com/mobdeve/s13/group8/nomadsapp/Register.java
+++ app/src/main/java/com/mobdeve/s13/group8/nomadsapp/Register.java
@@ -1,33 +1,56 @@
 package com.mobdeve.s13.group8.nomadsapp;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.OpenableColumns;
 import android.text.Spannable;
 import android.text.SpannableString;
 import android.text.method.LinkMovementMethod;
 import android.text.style.ClickableSpan;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 import com.mobdeve.s13.group8.nomadsapp.databinding.ActivityRegisterBinding;
 
 import java.net.URI;
 import java.net.URL;
+import java.util.UUID;
 
 public class Register extends AppCompatActivity {
 
+    private TextView file;
+    private ImageButton imageBtn;
+    public Uri profileUri, uploadedUri;
+    private FirebaseStorage profileStorage;
+    private StorageReference profileStorageReference;
+
+    @SuppressLint("MissingInflatedId")
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
         ActivityRegisterBinding viewBinding = ActivityRegisterBinding.inflate(getLayoutInflater());
         setContentView(viewBinding.getRoot());
+
+        profileStorage=FirebaseStorage.getInstance();
+        profileStorageReference=profileStorage.getReference();
+
+        imageBtn= findViewById(R.id.imageBtn);
+        file= findViewById(R.id.file);
 
         // redirect to login
         TextView textView = findViewById(R.id.redirectLogin);
@@ -47,6 +70,16 @@ public class Register extends AppCompatActivity {
 
         textView.setText(spannableString);
         textView.setMovementMethod(LinkMovementMethod.getInstance());
+
+        //upload profile
+        imageBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                choosePicture();
+            }
+        });
+
+
 
         // register button
         viewBinding.registerBtn.setOnClickListener(new View.OnClickListener() {
@@ -77,7 +110,8 @@ public class Register extends AppCompatActivity {
                                                 viewBinding.passwordIn.getText().toString(),
                                                 viewBinding.emailIn.getText().toString(),
                                                 // set default imageUri
-                                                "android.resource://com.mobdeve.s13.group8.nomadsapp/drawable/user_profile"
+                                                uploadedUri.toString()
+                                                //"android.resource://com.mobdeve.s13.group8.nomadsapp/drawable/user_profile"
                                                 );
                                         Toast.makeText(Register.this, viewBinding.emailIn.getText().toString(), Toast.LENGTH_SHORT).show();
                                         // Add the user to the database
@@ -106,4 +140,74 @@ public class Register extends AppCompatActivity {
             }
         });
     }
+
+    private void choosePicture() {
+        Intent intent= new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent, 1);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == 1 && resultCode == RESULT_OK && data!=null && data.getData()!=null){
+            profileUri = data.getData();
+            imageBtn.setImageURI(profileUri);
+            uploadPicture();
+        }
+    }
+
+    private void uploadPicture() {
+        final ProgressDialog pd = new ProgressDialog(this);
+        pd.setTitle("Uploading...");
+        pd.show();
+
+        final String randomKey = UUID.randomUUID().toString();
+        StorageReference riverRef = profileStorageReference.child("images/" + randomKey);
+
+
+        String fileName= getFileNameFromUri(profileUri);
+        file.setText(fileName);
+
+        riverRef.putFile(profileUri)
+                .addOnSuccessListener(taskSnapshot -> {
+                    pd.dismiss();
+                    Snackbar.make(findViewById(android.R.id.content), "Image Uploaded.", Snackbar.LENGTH_LONG).show();
+
+                    // Get the download URL from the uploaded image
+                    riverRef.getDownloadUrl().addOnSuccessListener(uri -> {
+                        uploadedUri= uri;
+
+                    });
+                })
+                .addOnFailureListener(e -> {
+                    pd.dismiss();
+                    Toast.makeText(getApplicationContext(), "Failed to Upload", Toast.LENGTH_LONG).show();
+                })
+                .addOnProgressListener(snapshot -> {
+                    double progressPercent = (100.00 * snapshot.getBytesTransferred() / snapshot.getTotalByteCount());
+                    pd.setMessage("Percentage: " + (int) progressPercent + "%");
+                });
+    }
+
+    private String getFileNameFromUri(Uri uri) {
+        String result = null;
+        if (uri.getScheme().equals("content")) {
+            try (Cursor cursor = getContentResolver().query(uri, null, null, null, null)) {
+                if (cursor != null && cursor.moveToFirst()) {
+                    int index = cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME);
+                    if (index != -1) {
+                        result = cursor.getString(index);
+                    }
+                }
+            }
+        }
+        if (result == null) {
+            result = uri.getLastPathSegment();
+        }
+        return result;
+    }
+
+
 }
