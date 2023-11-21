de0913c24363671f1713687e97abf1bc6167d520
alyssaysabelle <85222862+alyssaysabelle@users.noreply.github.com>
2023-11-21 11:21:51 +0800

    - added firestore reference for post

--- app/src/main/java/com/mobdeve/s13/group8/nomadsapp/MyFirestoreReferences.java
+++ app/src/main/java/com/mobdeve/s13/group8/nomadsapp/MyFirestoreReferences.java
@@ -3,6 +3,7 @@ package com.mobdeve.s13.group8.nomadsapp;
 public class MyFirestoreReferences {
     public final static String
             USERS_COLLECTION = "Users",
+            POSTS_COLLECTION = "Posts",
             USERNAME_FIELD = "username",
             FULLNAME_FIELD = "fullname",
             EMAIL_FIELD = "email",
