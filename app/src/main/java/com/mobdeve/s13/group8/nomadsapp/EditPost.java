949a19bbc1322b9a7ab11135430a00d278a2bbe1
Richard <94967357+lcdApple@users.noreply.github.com>
2023-11-24 11:57:45 +0800

    Add Upload Image Logic in EditPost

--- app/src/main/java/com/mobdeve/s13/group8/nomadsapp/EditPost.java
+++ app/src/main/java/com/mobdeve/s13/group8/nomadsapp/EditPost.java
@@ -1,11 +1,15 @@
 package com.mobdeve.s13.group8.nomadsapp;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.OpenableColumns;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
@@ -14,6 +18,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
@@ -23,6 +28,7 @@ import com.squareup.picasso.Picasso;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
+import java.util.UUID;
 
 public class EditPost extends AppCompatActivity {
     private EditText locationEt, captionEt, bodyEt;
@@ -33,7 +39,7 @@ public class EditPost extends AppCompatActivity {
     private User currentUser;
     private Post currentPost;
     private ImageButton imageBtn;
-    //public Uri postUri;
+    public Uri postUri;
     private FirebaseStorage postStorage;
     private StorageReference postStorageReference;
 
@@ -58,6 +64,7 @@ public class EditPost extends AppCompatActivity {
         locationEt = findViewById(R.id.locationEt);
         bodyEt = findViewById(R.id.bodyEt);
         imageView = findViewById(R.id.imageView);
+        file = findViewById(R.id.file);
 
         FirebaseFirestore db = FirebaseFirestore.getInstance();
         // query for getting post based on id
@@ -89,6 +96,12 @@ public class EditPost extends AppCompatActivity {
             }
         });
 
+        imageBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                choosePicture();
+            }
+        });
         //upload image button
         /*imageBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -159,5 +172,73 @@ public class EditPost extends AppCompatActivity {
             }
         });
 
+    }
+    private void choosePicture() {
+        Intent intent= new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent, 1);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == 1 && resultCode == RESULT_OK && data!=null && data.getData()!=null){
+            postUri = data.getData();
+            imageBtn.setImageURI(postUri);
+            uploadPicture();
+        }
+    }
+
+    private void uploadPicture() {
+        final ProgressDialog pd = new ProgressDialog(this);
+        pd.setTitle("Uploading...");
+        pd.show();
+
+        final String randomKey = UUID.randomUUID().toString();
+        StorageReference riverRef = postStorageReference.child("images/" + randomKey);
+
+
+        String fileName= getFileNameFromUri(postUri);
+        file.setText(fileName);
+
+        riverRef.putFile(postUri)
+                .addOnSuccessListener(taskSnapshot -> {
+                    pd.dismiss();
+                    Snackbar.make(findViewById(android.R.id.content), "Image Uploaded.", Snackbar.LENGTH_LONG).show();
+
+                    // Get the download URL from the uploaded image
+                    riverRef.getDownloadUrl().addOnSuccessListener(uri -> {
+                        imageUri = uri;
+                        Picasso.get().load(uri).into(imageView);
+                    });
+                })
+                .addOnFailureListener(e -> {
+                    pd.dismiss();
+                    Toast.makeText(getApplicationContext(), "Failed to Upload", Toast.LENGTH_LONG).show();
+                })
+                .addOnProgressListener(snapshot -> {
+                    double progressPercent = (100.00 * snapshot.getBytesTransferred() / snapshot.getTotalByteCount());
+                    pd.setMessage("Percentage: " + (int) progressPercent + "%");
+                });
+    }
+
+
+    private String getFileNameFromUri(Uri uri) {
+        String result = null;
+        if (uri.getScheme().equals("content")) {
+            try (Cursor cursor = getContentResolver().query(uri, null, null, null, null)) {
+                if (cursor != null && cursor.moveToFirst()) {
+                    int index = cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME);
+                    if (index != -1) {
+                        result = cursor.getString(index);
+                    }
+                }
+            }
+        }
+        if (result == null) {
+            result = uri.getLastPathSegment();
+        }
+        return result;
     }
 }
